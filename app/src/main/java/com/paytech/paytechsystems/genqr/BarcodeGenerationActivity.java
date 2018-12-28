cbfcd1ca33bc8cb3d2723a0801f902f65e216f5d
fredrick.kimayos <ghafred@gmail.com>
2018-12-28 13:35:39 +0300

    Commit Barcode

--- app/src/main/java/com/paytech/paytechsystems/genqr/BarcodeGenerationActivity.java
+++ app/src/main/java/com/paytech/paytechsystems/genqr/BarcodeGenerationActivity.java
@@ -3,6 +3,8 @@ package com.paytech.paytechsystems.genqr;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,8 +12,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.paytech.paytechsystems.R;
 import net.glxn.qrgen.android.QRCode;
+
+import java.util.Arrays;
 
 public class BarcodeGenerationActivity extends AppCompatActivity implements View.OnClickListener {
     private  EditText contentEditText;
@@ -44,8 +53,53 @@ public class BarcodeGenerationActivity extends AppCompatActivity implements View
                 Toast.makeText(this, "Enter something to create a barcode", Toast.LENGTH_SHORT).show();
                 return;
             }
-            Bitmap bitmap = QRCode.from(text).withColor(0xFFFF0000, 0xFFFFFFAA).withSize(1000, 1000).bitmap();
-            generationImageView.setImageBitmap(bitmap);
+            //Bitmap bitmap = QRCode.from(text).withColor(0xFFFF0000, 0xFFFFFFAA).withSize(1000, 1000).bitmap();
+            //generationImageView.setImageBitmap(bitmap);
+            try {
+                createBarcodeBitmap(text, 2,1);
+            }catch (WriterException e){
+                Toast.makeText(this, "Barcode Error :" + e, Toast.LENGTH_SHORT).show();
+            }
         }
     }
+
+    private void createBarcodeBitmap(String data, int width, int height) throws WriterException {
+        MultiFormatWriter writer = new MultiFormatWriter();
+        String finalData = Uri.encode(data);
+
+        // Use 1 as the height of the matrix as this is a 1D Barcode.
+        BitMatrix bm = writer.encode(finalData, BarcodeFormat.CODE_128, width, 1);
+        int bmWidth = bm.getWidth();
+
+        Bitmap imageBitmap = Bitmap.createBitmap(bmWidth, height, Bitmap.Config.ARGB_8888);
+
+        for (int i = 0; i < bmWidth; i++) {
+            // Paint columns of width 1
+            int[] column = new int[height];
+            Arrays.fill(column, bm.get(i, 0) ? Color.BLACK : Color.WHITE);
+            imageBitmap.setPixels(column, 0, 1, i, 0, 1, height);
+        }
+        generationImageView.setImageBitmap(imageBitmap);
+        ///return imageBitmap;
+    }
+
+//    private Bitmap createBarcodeBitmap(String data, int width, int height) throws WriterException {
+//        MultiFormatWriter writer = new MultiFormatWriter();
+//        String finalData = Uri.encode(data);
+//
+//        // Use 1 as the height of the matrix as this is a 1D Barcode.
+//        BitMatrix bm = writer.encode(finalData, BarcodeFormat.CODE_128, width, 1);
+//        int bmWidth = bm.getWidth();
+//
+//        Bitmap imageBitmap = Bitmap.createBitmap(bmWidth, height, Config.ARGB_8888);
+//
+//        for (int i = 0; i < bmWidth; i++) {
+//            // Paint columns of width 1
+//            int[] column = new int[height];
+//            Arrays.fill(column, bm.get(i, 0) ? Color.BLACK : Color.WHITE);
+//            imageBitmap.setPixels(column, 0, 1, i, 0, 1, height);
+//        }
+//
+//        return imageBitmap;
+//    }
 }
